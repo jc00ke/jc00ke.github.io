22cb90648d48a92dd752bc4eb764f27b79dc7bea
Jesse Cooke <jesse@maynard.(none)>
2009-08-20 17:55:27 -0700

    Added email addy

--- site.rb
+++ site.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-	'It gets better, just give it some time'
+	'It gets better, just give it some time<br />But for now you can get a hold of me via: work at jc00ke dot com'
 end
