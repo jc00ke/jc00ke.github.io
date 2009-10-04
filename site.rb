0672d4296e83aac07d1abc24492d104b4a2c1b6a
Jesse Cooke <jesse@maynard.(none)>
2009-10-04 14:35:56 -0700

    Adding Ubuntu 9.10 countdown badge

--- site.rb
+++ site.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-	'It gets better, just give it some time<br />But for now you can get a hold of me via: work at jc00ke dot com'
+	'It gets better, just give it some time<br />But for now you can get a hold of me via: work at jc00ke dot com<br /><script type="text/javascript" src="http://www.ubuntu.com/files/countdown/display2.js"></script>'
 end
