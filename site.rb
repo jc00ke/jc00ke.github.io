5fee7989d29c0281cf46acc0ffa313443a484080
jc00ke <jesse@maynard.(none)>
2009-10-13 15:19:41 -0700

    Making html5 and adding Google site verification

--- site.rb
+++ site.rb
@@ -2,5 +2,11 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-	'It gets better, just give it some time<br />But for now you can get a hold of me via: work at jc00ke dot com<br /><script type="text/javascript" src="http://www.ubuntu.com/files/countdown/display2.js"></script>'
+	body = '<!DOCTYPE html><html>'
+	body << '<head><title>jc00ke llc</title>'
+	body << '<meta name=\"google-site-verification\" content=\"2OjD3SXVlbH70lZl5vUDKKDEtqUcwQ2sSfaW91C-0O4\" >'
+	body << '</head><body>'
+	body << 'It gets better, just give it some time<br />But for now you can get a hold of me via: work at jc00ke dot com<br /><script src="http://www.ubuntu.com/files/countdown/display2.js"></script>'
+	body << '</body></html>'
+	body
 end
