bf0cff885136d184501c9d4ad6f36c08897b92e3
jc00ke <jesse@jc00ke.com>
2009-12-05 23:41:33 -0800

    Moving ahead with basic site

--- site.rb
+++ site.rb
@@ -1,12 +1,13 @@
-require 'rubygems'
-require 'sinatra'
+%w(rubygems sinatra haml rack-flash).each { |r| require r }
+
+set :haml,      { :format => :html5 }
+set :sessions,  true
+use Rack::Flash,    :accessorize => [ :notice, :error ]
 
 get '/' do
-	body = '<!DOCTYPE html><html>'
-	body << '<head><title>jc00ke llc</title>'
-	body << '<meta name="google-site-verification" content="2OjD3SXVlbH70lZl5vUDKKDEtqUcwQ2sSfaW91C-0O4" >'
-	body << '</head><body>'
-	body << 'It gets better, just give it some time<br />But for now you can get a hold of me via: work at jc00ke dot com<br /><script src="http://www.ubuntu.com/files/countdown/display2.js"></script>'
-	body << '</body></html>'
-	body
+    haml :index
+end
+
+get '/styles.css' do
+    content_type 'text/css', :charset => 'utf-8'
 end
