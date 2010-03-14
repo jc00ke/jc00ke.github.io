ef149d77fbb9ee5a33c32fbf0d22a255ea5d0af5
jc00ke <jesse@jc00ke.com>
2010-03-13 17:51:23 -0800

    Adding resume

--- site.rb
+++ site.rb
@@ -1,16 +1,38 @@
-%w(rubygems sinatra/base haml sass rack-flash yaml).each { |r| require r }
+%w(rubygems sinatra/base haml sass rack-flash yaml json).each { |r| require r }
 
 class Site < Sinatra::Base
 
     set :haml,          { :format => :html5 }
     set :sessions,      true
     enable :static
-    use Rack::Static,   :urls => %w(/images /javascripts), :root => 'public'
+    use Rack::Static,   :urls => %w(/images /javascripts /docs), :root => 'public'
     use Rack::Flash,    :accessorize => [ :notice, :error ]
 
     configure :development do
         Sinatra::Application.reset!
         use Rack::Reloader
+    end
+
+    before do
+
+        @title =    case request.path_info
+                    when '/'
+                        'Welcome!'
+                    when '/resume'
+                        'My Resume/CV/Experience'
+                    when '/contact'
+                        "Let's chat"
+                    else
+                        'Where are you?'
+                    end
+    end
+
+    helpers do
+
+        def get_yaml
+            File.open('public/docs/resume.yml') { |y| YAML::load y }
+        end
+
     end
 
     get '/' do
@@ -23,9 +45,20 @@ class Site < Sinatra::Base
     end
 
     get '/resume' do
-        @resume = File.open('public/resume.yml') { |y| YAML::load y }
+        @resume = get_yaml
         @info   = @resume['info']
         haml :resume
+    end
+
+    get %r{/resume.(yml|json|pdf)} do |ft|
+        if ft == 'yml' || ft == 'pdf'
+            send_file "public/docs/resume.#{ft}"
+        else
+            yml     = get_yaml
+            content_type 'text/json'
+            attachment 'resume.json'
+            JSON.pretty_generate yml
+        end
     end
 
     get '/contact' do
